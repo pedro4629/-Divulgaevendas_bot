c4b4ba4fab78b0839d78f922ba578c2fb609431f
pedro4629 <Pedroheriquemoreiracardoso@gmail.com>
2026-01-18 23:11:36 -0300

    Delete handlers/handlers directory

--- handlers/handlers/menu.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📋 Menu principal")
